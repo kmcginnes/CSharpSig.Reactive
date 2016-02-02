7c52224179d6515893976bba3aab9b4924434419
Kris McGinnes <kmcginnes@me.com>
2016-02-01 23:29:36 -0600

    Move RxUI extensions to ReactiveUiExtensions

--- ImprovingU.Reactive/Infrastructure/Extensions.cs
+++ ImprovingU.Reactive/Infrastructure/Extensions.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
-using System.Reactive.Linq;
 
-namespace ImprovingU.Reactive.Infrastructure
+namespace System.Reactive.Linq
 {
     public static class Extensions
     {
@@ -14,25 +12,6 @@ namespace ImprovingU.Reactive.Infrastructure
             TResult defaultValue = default (TResult))
         {
             return that != null ? selector(that) : defaultValue;
-        }
-
-        /// <summary>
-        /// Aggregates the items in the observable sequence into a readonly list.
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="source"></param>
-        /// <returns></returns>
-        public static IObservable<IReadOnlyList<T>> ToReadOnlyList<T>(this IObservable<T> source)
-        {
-            return source.ToList().Select(list => new ReadOnlyCollection<T>(list));
-        }
-
-        /// <summary>
-        /// Aggregates the items in the observable sequence into a readonly list.
-        /// </summary>
-        public static IObservable<IReadOnlyList<TResult>> ToReadOnlyList<T, TResult>(this IObservable<IEnumerable<T>> source, Func<T, TResult> map)
-        {
-            return source.Select(items => new ReadOnlyCollection<TResult>(items.Select(map).ToList()));
         }
     }
 }
---
+++ ImprovingU.Reactive/Infrastructure/ReactiveUiExtensions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Windows.Controls;
+
+namespace ReactiveUI
+{
+    public static class ReactiveUiExtensions
+    {
+        public static IDisposable BindPassword<TViewModel, TView, TVMProp, TVProp>(this TView view,
+            TViewModel viewModel,
+            Expression<Func<TViewModel, TVMProp>> vmProperty,
+            Expression<Func<TView, TVProp>> viewProperty,
+            PasswordBox passwordBox) where TViewModel : class where TView : IViewFor
+        {
+            var oneWayBind = view.OneWayBind(viewModel, vmProperty, viewProperty);
+            var bindTo = passwordBox.Events().PasswordChanged
+                .Select(_ => passwordBox.Password)
+                .BindTo(viewModel, vmProperty);
+            return new CompositeDisposable(oneWayBind, bindTo);
+        }
+
+        /// <summary>
+        /// Aggregates the items in the observable sequence into a readonly list.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static IObservable<IReadOnlyList<T>> ToReadOnlyList<T>(this IObservable<T> source)
+        {
+            return source.ToList().Select(list => new ReadOnlyCollection<T>(list));
+        }
+
+        /// <summary>
+        /// Aggregates the items in the observable sequence into a readonly list.
+        /// </summary>
+        public static IObservable<IReadOnlyList<TResult>> ToReadOnlyList<T, TResult>(this IObservable<IEnumerable<T>> source, Func<T, TResult> map)
+        {
+            return source.Select(items => new ReadOnlyCollection<TResult>(items.Select(map).ToList()));
+        }
+    }
+}
